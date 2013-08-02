a1fa1ff9c58488f60ce48abbee584b4d136ce6a0
bmarquis <bmarquismarkail@users.noreply.github.com>
2013-08-02 01:02:37 +0000

    MarquisWorld 	setActiveScene: Removed the break on the default case and updated the string that occurs on unknown values SceneManager 	addScene: Optimized error code. MarquisScene 	addObject: added setError code ObjectManager 	addObject: added error code to the function if Object points to NULL or the ID of the Object is already in the list.
    Misc
    	Added some comments in various files for future teammembers.

--- Scene/marquisscene.cpp
+++ Scene/marquisscene.cpp
@@ -3,7 +3,6 @@
 MarquisScene::MarquisScene(const sc_id &id, WorldBase *World) : SceneBase(id)
 {
     inWorld = World;
-    //ctor
 }
 
 MarquisScene::~MarquisScene()
@@ -18,11 +17,25 @@ WorldBase* MarquisScene::getParent()
 
 int MarquisScene::addObject(ObjectBase *Object)
 {
-    return objManager->addObject(Object);
+    switch(objManager->addObject(Object))
+	{
+		case 0:
+			return 0;
+		case -1:
+			setError("addObject: The Object you are trying to insert has an ID already in the Object List.");
+			return -1;
+		case -2:
+			setError("addObject: The Object points to empty memory.");
+			return -2;
+		default:
+			setError("addScene: An unknown error occurred.", true);
+	}
+	
 }
 
 int MarquisScene::deleteObject(ob_id ID)
 {
+	//TODO: add error messages 
     return objManager->deleteObject(ID);
 }
 
--- Scene/scenebase.h
+++ Scene/scenebase.h
@@ -9,19 +9,17 @@ typedef std::string sc_id; //the ID of the scene
 class SceneBase : virtual public _object
 {
     public:
-        //ctors and dtors
-        SceneBase(const sc_id &id);
-        virtual ~SceneBase();
-        //various other functions
-        virtual sc_id execute() = 0;            //updates the scene
-        virtual void close();               //cleans up the scene safely for closing or shifting of scenes.
+        SceneBase(const sc_id &id);				//The constructor. assigns an ID to the Scene
+        virtual ~SceneBase();					//The deconstructor.
+        //
+        virtual sc_id execute() = 0;            //Executes the scene.
+        virtual void close();               	//Cleans up the scene safely for closing or shifting of scenes.
         ObjectManager *getObjectManager();
-        sc_id getSceneID();                 //Gets the SceneID of the Scene
+        sc_id getSceneID();                 	//Gets the ID of the Scene
     protected:
-        ObjectManager *objManager;          //The Game Object Manager
-         //WorldBase *pWorld;                //The pointer to the scene's world
+        ObjectManager *objManager;          	//The Game Object Manager
     private:
-        sc_id ID;
+        sc_id ID;								//The Scene ID.
 };
 
 #endif // SCENEBASE_H
