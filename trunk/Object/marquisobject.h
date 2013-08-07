bd4b922a182605f1c9f89162f1d9ae09d94dd31b
bmarquis <bmarquis@e59e529a-72a6-4125-9225-a92ea1be70ce>
2013-08-07 07:57:06 +0000

    Minor Update - Added forward declarations

--- trunk/Object/marquisobject.h
+++ trunk/Object/marquisobject.h
@@ -2,15 +2,17 @@
 #define MARQUISOBJECT_H
 
 #include "objectbase.h"
-#include "../Scene/marquisscene.h"
 #include "../Component/marquiscomponent.h"
+#include "../Scene/marquisscene.h"
 
+class MarquisComponent;
+class MarquisScene;
 
 class MarquisObject : public ObjectBase
 {
     public:
 		//The constructor. assigns an objectID and the scene that will execute this object
-        MarquisObject(const ob_id &id, MarquisScene *Scene);	
+        MarquisObject(const ob_id &id, MarquisScene *Scene);
         ~MarquisObject();									//The deconstructor. performs cleanup.
         int addComponent(MarquisComponent *Cmpnt);				//Adds a component into the componentlist.
         int deleteComponent(com_id ID);						//deletes a componet from the componentlist.
--- trunk/Scene/marquisscene.h
+++ trunk/Scene/marquisscene.h
@@ -5,15 +5,16 @@
 #include "../World/marquisworld.h"
 #include "../Object/marquisobject.h"
 
-
+class MarquisObject;
+class MarquisWorld;
 
 class MarquisScene : public SceneBase
 {
     public:
 		//The constructor. assigns an ID and the world where it is being used (or null if not used yet)
 		//TODO: Make Assign to World function.
-        MarquisScene(const sc_id &id, MarquisWorld *World);   
-        ~MarquisScene();									//The deconstructor. 
+        MarquisScene(const sc_id &id, MarquisWorld *World);
+        ~MarquisScene();									//The deconstructor.
         MarquisWorld *getParent();								//returns a pointer to the parent World.
         int addObject(MarquisObject *Object);					//adds an Object in the scene's objectlist.
         int deleteObject(ob_id ID);							//delete's an object from the scene's objectlist.
